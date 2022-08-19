5c8239bb576d5f3887671a2062775d81f37cb49b
jmhar <jmhar@protonmail.com>
2022-08-20 00:58:13 +1000

    install packages

--- src/hooks/api/useGeolocation.ts
+++ src/hooks/api/useGeolocation.ts
@@ -1,6 +1,13 @@
-import { useMemo, useCallback } from "react";
+import { useEffect, useMemo, useState } from "react";
+
+export interface CurrentLocation {
+ longitude: number,
+ latitude: number,
+}
 
 export const useGeolocation = () => {
+  const [currentLocation, setCurrentLocation] = useState<CurrentLocation>();
+
   const geolocationPermissions = useMemo(() => {
     if ("geolocation" in navigator) {
       return true;
@@ -9,16 +16,16 @@ export const useGeolocation = () => {
     }
   }, [navigator]);
 
-  const currentLocation = useCallback(() => {
+  useEffect(() => {
     if (geolocationPermissions) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        return {
+      return navigator.geolocation.getCurrentPosition((position) => {
+        setCurrentLocation({
           latitude: position.coords.latitude,
           longitude: position.coords.longitude,
-        };
+        });
       });
     }
-  }, [geolocationPermissions, navigator]);
+  }, [geolocationPermissions, navigator, setCurrentLocation]);
 
   return { geolocationPermissions, currentLocation };
 };
